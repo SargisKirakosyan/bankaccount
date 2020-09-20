a9ddaa4bf322c185887da538f6fab6213343542f
SargisKirakosyan <sargiskirakosyan.wrk@gmail.com>
2020-09-20 23:20:29 +0400

    Update account.h

--- account.h
+++ account.h
@@ -1,1 +1,34 @@
+#pragma once
 
+#include "json.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <fstream>
+#include <string>
+
+using json = nlohmann::json;
+
+void showMainMenu();
+void menuListNumber();
+
+namespace bankAccount {
+  class Person {
+    public:
+      std::string firstName;
+      std::string lastName;
+      int birthDay;
+      int birthMonth;
+      int birthYear;
+      int accountBalance;
+      std::string moneyCurrency;
+      char registrationDate[50];
+      void createAccount();
+      void updateAccount();
+      void makeTransaction();
+      void accountDetails();
+      void removeAccount();
+      void customersLeft();
+      void exitToMenu();
+  };
+}
